4935b0f675f6b1571aab679461dd024ccbfcba3b
Glitch (hallwayrush) <none>
2018-03-25 19:13:08 +0000

    🎮🍃 Checkpoint ./script.js:789110/43:789154/653:789170/2231 ./hallway.js:789139/162

--- hallway.js
+++ hallway.js
@@ -1,5 +1,7 @@
+/* global createCanvas, line, mouseIsPressed, fill, mouseX, mouseY, ellipse , angleMode , DEGREES */
 function setup()
 {
     createCanvas(400, 400);   
     angleMode(DEGREES);
+    line();
 }
--- script.js
+++ script.js
@@ -1,31 +1,23 @@
-var change = {
-  37: {
-    left: "-=1"
-  },
-  39: {
-    left: "+=1"
-  }
-}
-$(document).one("keydown", keyDown)
+var container = $('#container'),
+    character = $('#character'),
+    w = container.width() - character.width(),
+    d = {},
+    x = 10;
 
-var going;
-
-function keyDown(e) {
-  $(document).one("keyup", keyup)
-  var animation = change[e.which];
-  going = setInterval(keepGoing, 1);
-  function keepGoing() 
-    if (($("#character").offset().left < 100) {
-        $("#character").css(animation)
-      }
-  }
+function newv(v,a,b) {
+    var n = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0);
+    return n < 0 ? 0 : n > w ? w : n;
 }
 
-function keyup(e) {
-  clearInterval(going)
-  $(document).one("keydown", keyDown)
-}
+$(window).keydown(function(e) { d[e.which] = true; });
+$(window).keyup(function(e) { d[e.which] = false; });
 
+setInterval(function() {
+    character.css({
+        left: function(i,v) { return newv(v, 37, 38); },
+        
+    });
+}, 20);
 
 
 
