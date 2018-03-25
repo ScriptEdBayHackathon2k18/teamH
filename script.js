39b199f8508aaa940e32ae22c6e125d1b4870991
Glitch (hallwayrush) <none>
2018-03-25 18:51:58 +0000

    🍬🐄 Checkpoint ./index.html:789139/26 ./script.js:789154/722:789170/25 ./styles.css:789110/10:789139/76:789170/57 ./page-two.js:789139/41 ./page-two.html:789139/67

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-console.log('hi');
-$("#character")
+var change = {
+  37: {
+    left: "-=1"
+  },
+  39: {
+    left: "+=1"
+  }
+}
+$(document).one("keydown", keyDown)
 
+var going;
 
+function keyDown(e) {
+  $(document).one("keyup", keyup)
+  var animation = change[e.which];
+  going = setInterval(keepGoing, 1);
+  function keepGoing() {
+    $("#character").css(animation)
+  }
+}
 
+function keyup(e) {
+  clearInterval(going)
+  $(document).one("keydown", keyDown)
+}
 
 
 
